6a6941bd6bc1496fbb403e7f2fef4de142752566
Piti [Savati] <amoeba_wp@live.fr>
2023-03-24 18:30:41 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,12 +5,12 @@ const nextConfig = {
     name: 'Pt Workspaces',
     description: 'Personal registry of Pt custom Kasm Workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://Amoebawp.github.io/kasm-repo_testing/',
-    contactUrl: '',
+    listUrl: 'https://amoebawp.github.io/kasm-repo_testing/',
+    contactUrl: 'https://amoebawp.github.io/kasm-repo_testing/',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  /*basePath: '/kasm-registry/1.0',*/
   trailingSlash: true,
   images: {
     unoptimized: true,
