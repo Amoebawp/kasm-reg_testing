4ec6ce0ed56d9ecd4495040dd3905b8e64ed5f50
Piti [Savati] <amoeba_wp@live.fr>
2023-03-26 11:50:48 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Pt Workspaces',
-    description: 'Personal registry of Pt custom Kasm Workspaces',
-    icon: '/img/logo.svg',
+    name: 'Fanthi Kasm Store',
+    description: 'Custom Kasm Workspaces',
+    icon: 'https://home.fanthi.fr/src/logo.png',
     listUrl: 'https://amoebawp.github.io/kasm-reg_testing/list.json',
     contactUrl: 'https://amoebawp.github.io/kasm-reg_testing/',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/1.0',
+  basePath: 'kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
